4f356c54f2f595cadfa2806759b0094ab3d05d69
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2023-05-14 15:15:16 +0700

    init db module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { DbModule } from './db/db.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, DbModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
